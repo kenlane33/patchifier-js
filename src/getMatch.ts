a87650264b7be54ad8e83d9da2c9c87a22533cf1
Ken <kenlane33@gmail.com>
2023-04-30 12:12:22 -0400

    getMatch() & tests working

--- src/getMatch.ts
+++ src/getMatch.ts
@@ -1,47 +1,71 @@
 import {get as _get } from 'lodash'
 
-function getMatch(obj: Record<string, unknown>, matchStr: string): unknown {
-  const [path, value] = matchStr.split(":")
-  const regExpStr = (value && value.match(/^\/(.*)\/$/)?.[1]) //=
-  const valueRegExp = regExpStr ? new RegExp(regExpStr) : undefined //=
-  const objValue = _get(obj, path); //=
-  if (value === undefined) {
-    return objValue;
-  } else {
-    if (valueRegExp) {
-      return valueRegExp.test(objValue as string) ? objValue : undefined;
-    } else {
-      return objValue+'' === value ? objValue : undefined;
-    }
-  }
+/**
+ * 
+ * Use matchStr path to get a matching deep value from an object.
+ * 
+ * @param obj - object to search
+ * @param matchStr - lodash path, then an optional : and value or regExp to match. Examples: 'a.b' or 'a.b:42' or 'a.b:/^4/'
+ * @returns matching object value, or undefined if no match
+ */
+export function getMatch(obj: Record<string, unknown>, matchStr: string): unknown {
+  if (matchStr=='') return obj
+  const [path, matchVal] = matchStr.split(":")
+  const objVal    = _get(obj, path)
+  if (matchVal === undefined) return objVal                         // example: getMatch({a:{b:42}},      'a.b') => 42
+  const regExpStr = matchVal && matchVal.match(/^\/(.*)\/$/)?.[1]
+  const regExp    = regExpStr && new RegExp(regExpStr)
+  const strVal    = objVal + ''
+  if (regExp) return (regExp.test(strVal))  ?  objVal  :  undefined // example: getMatch({a:{b:42}}, 'a.b:/^4/') => 42
+  else        return (strVal === matchVal)  ?  objVal  :  undefined // example: getMatch({a:{b:42}}, 'a.b:42'  ) => 42
 }
+
 const testObj = {
   a: {
     b: 42,
     c: 'hello',
     d: 'World',
-    t:true,
+    f: false,
+    t: true,
+    arr: [6,7,8]
   }
 };
 
-function chk( val, expectedVal ) {
-  return (val == expectedVal) ? ['PASS',val] : ['FAIL',val]
+function chkEql( a, b ) {
+  return (Array.isArray(a))   ?   
+    (a.length == b.length ) && a.some( (v,i) => v == b[i] ) 
+      :
+    (a == b)
 }
-
+function chk( a, b ) {
+  return (chkEql(a,b)) ? ['💚PASS', a] : ['❌FAIL', a]
+}
+function chkNot( val, expectedVal ) {  
+  return (chkEql(val, expectedVal)) ? ['🔴FAIL',val] : ['🟢PASS',val]
+}
 // matching exact value
-chk(getMatch(testObj, 'a.b:42'),    42       ) //=
-chk(getMatch(testObj, 'a.c:hello'), 'hello'  ) //=
-chk(getMatch(testObj, 'a.c:world'), null     ) //=
-chk(getMatch(testObj, 'a.t:true'),  true     ) //=
-
+chk(   getMatch(testObj, ''),          testObj  ) //=
+chk(   getMatch(testObj, 'a.b:42'),     42      ) //=
+chk(   getMatch(testObj, 'a.c:hello'), 'hello'  ) //=
+chk(   getMatch(testObj, 'a.c:world'), null     ) //=
+chk(   getMatch(testObj, 'a.t:true'),  true     ) //=
+chk(   getMatch(testObj, 'a.arr'),     [6,7,8]  ) //=
+chkNot(getMatch(testObj, 'a.arr'),     [6,7,8,9]) //=
 
 // matching with regex
+chk(getMatch(testObj, 'a.b:/.*/'),  42      ) //=
 chk(getMatch(testObj, 'a.d:/^W/'), 'World'  ) //=
-chk(getMatch(testObj, 'a.d:/^H/'), undefined) //=
+chk(getMatch(testObj, 'a.d:/^Q/'), undefined) //=
+chk(getMatch(testObj, 'a.b:/^4/'),  42      ) //=
+chk(getMatch(testObj, 'a.b://'  ), undefined) //=
 
 // matching without colon (any value matches)
-chk(getMatch(testObj, 'a.b'), 42       ) //=
-chk(getMatch(testObj, 'a.c'), 'hello'  ) //=
-chk(getMatch(testObj, 'a.d'), 'World'  ) //=
-chk(getMatch(testObj, 'a.e'),      null) //=
-chk(getMatch(testObj, 'a.e'), undefined) //=
+chk(   getMatch(testObj, 'a.b'),  42      ) //=
+chk(   getMatch(testObj, 'a.c'), 'hello'  ) //=
+chk(   getMatch(testObj, 'a.d'), 'World'  ) //=
+chk(   getMatch(testObj, 'a.t'), true     ) //=
+chk(   getMatch(testObj, 'a.f'), false    ) //=
+chkNot(getMatch(testObj, 'a.f'), null     ) //=
+chkNot(getMatch(testObj, 'a.f'), undefined) //=
+chk(   getMatch(testObj, 'a.e'),      null) //=
+chk(   getMatch(testObj, 'a.e'), undefined) //=
